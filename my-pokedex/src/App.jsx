a76b91cf20287c316322e4ed7d55bf75eb81d7b4
Marvindje <marvindje@wilder.school>
2023-06-04 00:33:02 +0200

    Ajouter les modifications pour le composant PokemonCard

--- my-pokedex/src/App.jsx
+++ my-pokedex/src/App.jsx
@@ -13,5 +13,32 @@ function App() {
     </div>
   );
 }
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+
+  return (
+    <div className="PokemonCard">
+      <figure>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
 
 export default App;
